08660e2cd0d10d266c5a31e1e9a1185d212951c6
autisticboy348 <gospodnikov9roman@gmail.com>
2026-06-23 14:46:35 +0300

    Create ContactTag.cs

---
+++ ContactTag.cs
@@ -0,0 +1,10 @@
+namespace EFCorePractice.Models;
+
+public class ContactTag
+{
+    public int ContactId { get; set; }
+    public Contact? Contact { get; set; }
+
+    public int TagId { get; set; }
+    public Tag? Tag { get; set; }
+}
